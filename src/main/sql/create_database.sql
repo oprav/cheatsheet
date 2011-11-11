b9507d696e93d7d826f0133a213f068b0a1cf2a4
Alex Vershynin <overshyn@softserveinc.com>
2011-11-11 15:37:21 +0200

    Added script which creates database.

---
+++ src/main/sql/create_database.sql
@@ -0,0 +1,23 @@
+﻿create sequence theme_id_seq;
+
+create table theme
+(
+	id int primary key default nextval('theme_id_seq'),
+	title varchar (100) not null
+	
+);
+
+alter sequence theme_id_seq owned by theme.id;
+
+create sequence topic_id_seq;
+
+create table topic
+(
+	id int primary key default nextval('topic_id_seq'),
+	theme_id int not null,
+	title varchar (500) not null,
+	sort_id int not null,
+	is_delete bit not null
+);
+
+alter sequence topic_id_seq owned by topic.id;
